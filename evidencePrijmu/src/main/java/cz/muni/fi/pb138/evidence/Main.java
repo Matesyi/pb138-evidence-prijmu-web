2d43c52750f58d030db3c3ebc3a0f577d9373464
Miloš Šilhár <migmig095@gmail.com>
2016-05-12 20:09:12 +0200

    Zjednodušenie kódu pre spustenie Jetty. Nastavenie dependencies, stránka presunutá do webapp.

---
+++ evidencePrijmu/src/main/java/cz/muni/fi/pb138/evidence/Main.java
@@ -0,0 +1,53 @@
+package cz.muni.fi.pb138.evidence;
+
+import java.net.URI;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.awt.Desktop;
+
+public class Main
+{
+    // Resource path pointing to where the WEBROOT is
+    private static final String RESOURCE_BASE = "src/main/webapp";
+    private static final String DESCRIPTOR_PATH = "src/main/webapp/WEB-INF/web.xml"; 
+    private static Server server;
+    
+    public static void main(String[] args) throws Exception
+    {
+        Main main = new Main();
+        
+        int serverPort = 8080;        
+        main.startJetty(serverPort);
+        
+        if (Desktop.isDesktopSupported()) {
+            Desktop.getDesktop().browse(new URI("http://localhost:" + serverPort + "/"));
+        }
+        
+        // stops server with any key
+        System.in.read();
+        main.stopJetty();
+    }
+    
+    private void startJetty(int port) throws Exception
+    {
+        server = new Server(port); // initialize server with port
+        
+        // set webapp context
+        WebAppContext webApp = new WebAppContext();
+        webApp.setDescriptor(DESCRIPTOR_PATH);
+        webApp.setResourceBase(RESOURCE_BASE);
+        
+        // set the context path of the app
+        webApp.setContextPath("/");
+        server.setHandler(webApp);
+        
+        // start server
+        server.start();
+    }
+    
+    private void stopJetty() throws Exception
+    {
+        server.stop();
+    }
+}
