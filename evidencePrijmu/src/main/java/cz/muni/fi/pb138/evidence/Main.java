7ad9bdfca60da2b4ce94ec261d5e49db14a60812
Hathoriel <lukas.kotol@gmail.com>
2016-06-03 19:49:20 +0200

    Filtering invoices + detail invoice done.

--- evidencePrijmu/src/main/java/cz/muni/fi/pb138/evidence/Main.java
+++ evidencePrijmu/src/main/java/cz/muni/fi/pb138/evidence/Main.java
@@ -1,6 +1,5 @@
 package cz.muni.fi.pb138.evidence;
 
-import cz.muni.fi.pb138.evidence.xmlEdit.ExecuteQuery;
 import java.net.URI;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -11,44 +10,42 @@ public class Main
 {
     // Resource path pointing to where the WEBROOT is
     private static final String RESOURCE_BASE = "src/main/webapp";
-    private static final String DESCRIPTOR_PATH = "src/main/webapp/WEB-INF/web.xml"; 
+    private static final String DESCRIPTOR_PATH = "src/main/webapp/WEB-INF/web.xml";
     private static Server server;
-    
+
     public static void main(String[] args) throws Exception
     {
         Main main = new Main();
-        
-        ExecuteQuery.initCollection();
-        
+
         int serverPort = 8090;
         main.startJetty(serverPort);
-        
+
         if (Desktop.isDesktopSupported()) {
             Desktop.getDesktop().browse(new URI("http://localhost:" + serverPort + "/"));
         }
-        
+
         // stops server with any key
         System.in.read();
         main.stopJetty();
     }
-    
+
     private void startJetty(int port) throws Exception
     {
         server = new Server(port); // initialize server with port
-        
+
         // set webapp context
         WebAppContext webApp = new WebAppContext();
         webApp.setDescriptor(DESCRIPTOR_PATH);
         webApp.setResourceBase(RESOURCE_BASE);
-        
+
         // set the context path of the app
         webApp.setContextPath("/");
         server.setHandler(webApp);
-        
+
         // start server
         server.start();
     }
-    
+
     private void stopJetty() throws Exception
     {
         server.stop();
--- evidencePrijmu/src/main/java/cz/muni/fi/pb138/evidence/xmlEdit/ExecuteQuery.java
+++ evidencePrijmu/src/main/java/cz/muni/fi/pb138/evidence/xmlEdit/ExecuteQuery.java
@@ -1,8 +1,6 @@
 package cz.muni.fi.pb138.evidence.xmlEdit;
 
-import java.io.File;
 import org.exist.xmldb.XQueryService;
-import org.exist.xmldb.XmldbURI;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
 import org.xmldb.api.base.Database;
@@ -10,48 +8,28 @@ import org.xmldb.api.base.Resource;
 import org.xmldb.api.base.ResourceIterator;
 import org.xmldb.api.base.ResourceSet;
 import org.xmldb.api.base.XMLDBException;
-import org.xmldb.api.modules.CollectionManagementService;
-import org.xmldb.api.modules.XMLResource;
 
 /**
- * Executes an extended XQuery. 
- * @author Lumir, Milos
+ * Executes an extended XQuery.
+ * @author Lumir
  */
 public class ExecuteQuery {
 
-    // required parameters
-    protected static String driver = "org.exist.xmldb.DatabaseImpl";
-    
     //database access parameters
+    protected static String driver = "org.exist.xmldb.DatabaseImpl";
     protected static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc";
-    protected static String username = "admin";
-    protected static String password = "";
-    
-    // path to initial workevidence xml
-    protected static String initResource = "../workEvidenceInit.xml";
-    
-    // collection path
     protected static String collectionPath = "/db/sample";
-  
-    private static Collection getDbCollection()
-            throws XMLDBException, ClassNotFoundException, InstantiationException, IllegalAccessException
-    {
+    protected static String resourceName = "workEvidence.xml";
+
+    public static String executeQuery(String xquery) throws XMLDBException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        //initialize database drivers
         Class cl = Class.forName(driver);
         Database database = (Database) cl.newInstance();
         DatabaseManager.registerDatabase(database);
-        Collection collection = 
-                DatabaseManager.getCollection(URI+collectionPath, username, password);
-        
-        return collection;
-    }
-    
-    public static String executeQuery(String xquery)
-            throws XMLDBException, ClassNotFoundException, InstantiationException, IllegalAccessException
-    {
-        Collection col = getDbCollection();
+        Collection collection = DatabaseManager.getCollection(URI+collectionPath);
 
         //executes XQuery
-        XQueryService service = (XQueryService) col.getService("XQueryService", "1.0");
+        XQueryService service = (XQueryService) collection.getService("XQueryService", "1.0");
         service.setProperty("indent", "yes");
         ResourceSet result = service.query(xquery);
         ResourceIterator i = result.getIterator();
@@ -63,41 +41,5 @@ public class ExecuteQuery {
             resultStr += value;
         }
         return resultStr;
-    }
-    
-    public static void initCollection()
-            throws XMLDBException, ClassNotFoundException, InstantiationException, IllegalAccessException
-    {		
-        // try to get collection
-        Collection col = getDbCollection();
-        
-        if(col == null) {
-            // collection does not exist: get root collection and create.
-            // for simplicity, we assume that the new collection is a
-            // direct child of the root collection, e.g. /db/test.
-            // the example will fail otherwise.
-            Collection root = DatabaseManager.getCollection(URI + XmldbURI.ROOT_COLLECTION, username, password);
-            CollectionManagementService mgtService = 
-                (CollectionManagementService)root.getService("CollectionManagementService", "1.0");
-            mgtService.createCollection(collectionPath.substring((XmldbURI.ROOT_COLLECTION + "/").length()));
-            
-            // puts default resource to db
-            putResource(initResource);
-        }
-    }
-    
-    public static void putResource(String fileName)
-            throws XMLDBException, ClassNotFoundException, InstantiationException, IllegalAccessException
-    {
-        Collection col = getDbCollection();
-            
-        File f = new File(fileName);
-        // create new XMLResource
-        XMLResource document = (XMLResource)col.createResource(f.getName(), "XMLResource");
-        document.setContent(f);
-        System.out.print("storing document " + document.getId() + "...");
-        col.storeResource(document);
-        System.out.println("ok.");
-        
     }
 }
